6f574c7abcace63dfb1da857b8c46e05e6c3f4b4
Likhitha <likhitha.sunkara@anblicks.com>
2023-06-24 17:06:42 +0530

    till pagination

--- OnlineShopping.WebApp/Controllers/ProductController.cs
+++ OnlineShopping.WebApp/Controllers/ProductController.cs
@@ -25,14 +25,72 @@ namespace OnlineShopping.WebApp.Controllers
             _mapper = mapper;
         }
 
-        public async Task<IActionResult> IndexProduct()
+        private List<SelectListItem> GetPageSizes(int selectedPageSize = 5)
+        {
+            var pagesSizes = new List<SelectListItem>();
+
+
+
+            if (selectedPageSize == 5)
+                pagesSizes.Add(new SelectListItem("5", "5", true));
+            else
+                pagesSizes.Add(new SelectListItem("5", "5"));
+
+
+
+            for (int lp = 10; lp <= 100; lp += 10)
+            {
+                if (lp == selectedPageSize)
+                { pagesSizes.Add(new SelectListItem(lp.ToString(), lp.ToString(), true)); }
+                else
+                    pagesSizes.Add(new SelectListItem(lp.ToString(), lp.ToString()));
+            }
+
+
+
+            return pagesSizes;
+        }
+        public async Task<IActionResult> IndexProduct(string SearchText = "", int pg = 1, int pageSize = 5)
         {
             List<ProductDto> list = new();
-
             var response = await _productService.GetAllAsync<APIResponse>(HttpContext.Session.GetString(SD.SessionToken));
             if (response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+                if (pg < 1) pg = 1;
+
+
+
+
+                if (SearchText != "" && SearchText != null)
+                {
+                    List<ProductDto> productList = new List<ProductDto>();
+                    foreach (var product in list)
+                    {
+                        if (product.Category.CategoryName.ToLower().Contains(SearchText.ToLower()) || product.ProductName.ToLower().Contains(SearchText.ToLower()))
+                            productList.Add(product);
+                    }
+                    list = productList;
+                }
+                int recsCount = list.Count();
+
+
+
+                int recSkip = (pg - 1) * pageSize;
+                var data = list.Skip(recSkip).Take(pageSize).ToList();
+
+
+
+                Pager SearchPager = new Pager(recsCount, pg, pageSize) { Action = "IndexProduct", Controller = "Product", SearchText = SearchText };
+                ViewBag.SearchPager = SearchPager;
+
+
+
+                this.ViewBag.PageSizes = GetPageSizes(pageSize);
+
+
+
+                return View(data.ToList());
             }
             return View(list);
         }
